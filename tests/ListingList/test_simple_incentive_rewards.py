fe7323a64ea88689e403ffdc057e79864402b817
SatoruKojo <147251757+SatoruKojo@users.noreply.github.com>
2023-10-29 02:34:38 +0900

    tests

--- tests/ListingList/test_simple_incentive_rewards.py
+++ tests/ListingList/test_simple_incentive_rewards.py
@@ -1,5 +1,5 @@
 import brownie
-from brownie import *
+from brownie import*
 
 def test_one_collection_incentive_reward_of_staker(accounts, finished_epoch):
 	(zooToken, daiToken, linkToken, nft) = finished_epoch[0]
@@ -17,22 +17,59 @@ def test_one_collection_incentive_reward_of_staker(accounts, finished_epoch):
 def test_no_rewards_after_finish_epoch(accounts, finished_epoch):
 	(zooToken, daiToken, linkToken, nft) = finished_epoch[0]
 	(vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
+    
+    tx = staking.claimIncentiveStakerReward(1, accounts[-1], {"from": accounts[0]})
+
+	assert tx.return_value > 0
+	assert zooToken.balanceOf(accounts[-1]) > 0
+    
+    while arena.currentEpoch() < arena.endEpochOfIncentiveRewards():
+        chain.sleep(arena.epochDuration() + 1)
+        chain.mine(1)
+        arena.updateEpoch({"from": accounts[0]})
+        tx = staking.claimIncentiveStakerReward(1, accounts[-1], {"from": accounts[0]})
+        assert tx.return_value > 0
+        
+    chain.sleep(arena.epochDuration() + 1)
+    chain.mine(1)
+    arena.updateEpoch({"from": accounts[0]})    
 
 	tx = staking.claimIncentiveStakerReward(1, accounts[-1], {"from": accounts[0]})
+	assert tx.return_value == 0
+ 
+ def test_one_collection_incentive_reward_of_voter(accounts, finished_epoch):
+	(zooToken, daiToken, linkToken, nft) = finished_epoch[0]
+	(vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
+
+	arena.updateInfoAboutVotingNumber(nft)
+	assert arena.numberOfVotingNftsInCollection(1, nft) > 0
+	assert arena.numberOfVotingNftsInCollection(2, nft) > 0
+
+	tx = voting.claimIncentiveVotingReward(1, accounts[-1], {"from": accounts[0]})
 
 	assert tx.return_value > 0
 	assert zooToken.balanceOf(accounts[-1]) > 0
 
-	while arena.currentEpoch() < arena.endEpochOfIncentiveRewards():
-		chain.sleep(arena.epochDuration() + 1)
-		chain.mine(1)
-		arena.updateEpoch({"from": accounts[0]})
-		tx = staking.claimIncentiveStakerReward(1, accounts[-1], {"from": accounts[0]})
-		assert tx.return_value > 0
+def test_intcentive_rewards_in_new_epoch(accounts, finished_epoch):
+	(zooToken, daiToken, linkToken, nft) = finished_epoch[0]
+	(vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
+    
+    tx = voting.claimIncentiveVotingReward(1, accounts[-1], {"from": accounts[0]})
 
-	chain.sleep(arena.epochDuration() + 1)
-	chain.mine(1)
-	arena.updateEpoch({"from": accounts[0]})
+	assert tx.return_value > 0
+	assert zooToken.balanceOf(accounts[-1]) > 0
+    
+    while arena.currentEpoch() < arena.endEpochOfIncentiveRewards():
+        chain.sleep(arena.epochDuration() + 1)
+        chain.mine(1)
+        arena.updateEpoch({"from": accounts[0]})
+        tx = voting.claimIncentiveVotingReward(1, accounts[-1], {"from": accounts[0]})
+        assert tx.return_value > 0
+        
+    chain.sleep(arena.epochDuration() + 1)
+    chain.mine(1)
+    arena.updateEpoch({"from": accounts[0]})    
 
-	tx = staking.claimIncentiveStakerReward(1, accounts[-1], {"from": accounts[0]})
+	tx = voting.claimIncentiveVotingReward(1, accounts[-1], {"from": accounts[0]})
 	assert tx.return_value == 0
+ 
--- tests/NftStakingPosition/test_claim_reward_from_staking.py
+++ tests/NftStakingPosition/test_claim_reward_from_staking.py
@@ -27,3 +27,12 @@ def test_emitting_event(accounts, tokens, battles):
 	stakingPositionId = 1
 
 	staking.claimRewardFromStaking(stakingPositionId, accounts[1], {"from": accounts[1]})
+ 
+def test_batch_claim_reward_from_staking(accounts, tokens, battles):
+	(zooToken, daiToken, linkToken, nft, lpZoo) = tokens
+	(vault, functions, governance, staking, voting, arena, listing) = battles
+
+	staking.stakeNft(nft.address, tokenId, {"from": accounts[1]})
+	stakingPositionId = 1
+
+	staking.batchClaimRewardFromStaking(stakingPositionId, accounts[1], {"from": accounts[1]})
