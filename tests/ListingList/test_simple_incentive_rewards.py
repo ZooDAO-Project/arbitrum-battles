e67f94eadecb55a24edd0be01894e174bdf6e1dd
SatoruKojo <147251757+SatoruKojo@users.noreply.github.com>
2023-10-29 03:22:43 +0900

    fix code

--- tests/ListingList/test_simple_incentive_rewards.py
+++ tests/ListingList/test_simple_incentive_rewards.py
@@ -2,26 +2,26 @@ import brownie
 from brownie import*
 
 def test_one_collection_incentive_reward_of_staker(accounts, finished_epoch):
-	(zooToken, daiToken, linkToken, nft) = finished_epoch[0]
-	(vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
+    (zooToken, daiToken, linkToken, nft) = finished_epoch[0]
+    (vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
 
-	arena.updateInfoAboutStakedNumber(nft)
-	assert arena.numberOfStakedNftsInCollection(1, nft) > 0
-	assert arena.numberOfStakedNftsInCollection(2, nft) > 0
+    arena.updateInfoAboutStakedNumber(nft)
+    assert arena.numberOfStakedNftsInCollection(1, nft) > 0
+    assert arena.numberOfStakedNftsInCollection(2, nft) > 0
 
-	tx = staking.claimIncentiveStakerReward(1, accounts[-1], {"from": accounts[0]})
+    tx = staking.claimIncentiveStakerReward(1, accounts[-1], {"from": accounts[0]})
 
-	assert tx.return_value > 0
-	assert zooToken.balanceOf(accounts[-1]) > 0
+    assert tx.return_value > 0
+    assert zooToken.balanceOf(accounts[-1]) > 0
 
 def test_no_rewards_after_finish_epoch(accounts, finished_epoch):
-	(zooToken, daiToken, linkToken, nft) = finished_epoch[0]
-	(vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
+    (zooToken, daiToken, linkToken, nft) = finished_epoch[0]
+    (vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
     
     tx = staking.claimIncentiveStakerReward(1, accounts[-1], {"from": accounts[0]})
 
-	assert tx.return_value > 0
-	assert zooToken.balanceOf(accounts[-1]) > 0
+    assert tx.return_value > 0
+    assert zooToken.balanceOf(accounts[-1]) > 0
     
     while arena.currentEpoch() < arena.endEpochOfIncentiveRewards():
         chain.sleep(arena.epochDuration() + 1)
@@ -34,30 +34,30 @@ def test_no_rewards_after_finish_epoch(accounts, finished_epoch):
     chain.mine(1)
     arena.updateEpoch({"from": accounts[0]})    
 
-	tx = staking.claimIncentiveStakerReward(1, accounts[-1], {"from": accounts[0]})
-	assert tx.return_value == 0
+    tx = staking.claimIncentiveStakerReward(1, accounts[-1], {"from": accounts[0]})
+    assert tx.return_value == 0
  
- def test_one_collection_incentive_reward_of_voter(accounts, finished_epoch):
-	(zooToken, daiToken, linkToken, nft) = finished_epoch[0]
-	(vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
+def test_one_collection_incentive_reward_of_voter(accounts, finished_epoch):
+    (zooToken, daiToken, linkToken, nft) = finished_epoch[0]
+    (vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
 
-	arena.updateInfoAboutVotingNumber(nft)
-	assert arena.numberOfVotingNftsInCollection(1, nft) > 0
-	assert arena.numberOfVotingNftsInCollection(2, nft) > 0
+    arena.updateInfoAboutVotingNumber(nft)
+    assert arena.numberOfVotingNftsInCollection(1, nft) > 0
+    assert arena.numberOfVotingNftsInCollection(2, nft) > 0
 
-	tx = voting.claimIncentiveVotingReward(1, accounts[-1], {"from": accounts[0]})
+    tx = voting.claimIncentiveVotingReward(1, accounts[-1], {"from": accounts[0]})
 
-	assert tx.return_value > 0
-	assert zooToken.balanceOf(accounts[-1]) > 0
+    assert tx.return_value > 0
+    assert zooToken.balanceOf(accounts[-1]) > 0
 
 def test_intcentive_rewards_in_new_epoch(accounts, finished_epoch):
-	(zooToken, daiToken, linkToken, nft) = finished_epoch[0]
-	(vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
+    (zooToken, daiToken, linkToken, nft) = finished_epoch[0]
+    (vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
     
     tx = voting.claimIncentiveVotingReward(1, accounts[-1], {"from": accounts[0]})
 
-	assert tx.return_value > 0
-	assert zooToken.balanceOf(accounts[-1]) > 0
+    assert tx.return_value > 0
+    assert zooToken.balanceOf(accounts[-1]) > 0
     
     while arena.currentEpoch() < arena.endEpochOfIncentiveRewards():
         chain.sleep(arena.epochDuration() + 1)
@@ -70,6 +70,5 @@ def test_intcentive_rewards_in_new_epoch(accounts, finished_epoch):
     chain.mine(1)
     arena.updateEpoch({"from": accounts[0]})    
 
-	tx = voting.claimIncentiveVotingReward(1, accounts[-1], {"from": accounts[0]})
-	assert tx.return_value == 0
- 
+    tx = voting.claimIncentiveVotingReward(1, accounts[-1], {"from": accounts[0]})
+    assert tx.return_value == 0
