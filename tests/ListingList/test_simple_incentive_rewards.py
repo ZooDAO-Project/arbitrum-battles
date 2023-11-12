e1ef15abdc0d9a00a84474039a98ff4728bdf8ce
Dmitry Korovin <korovin.ds@aurrency.com>
2023-11-12 04:21:53 +0300

    Fixed tests after pull requests

--- tests/ListingList/test_simple_incentive_rewards.py
+++ tests/ListingList/test_simple_incentive_rewards.py
@@ -1,5 +1,6 @@
 import brownie
-from brownie import*
+from brownie import *
+
 def test_one_collection_incentive_reward_of_staker(accounts, finished_epoch: tuple[tuple, tuple]):
     (zooToken, daiToken, linkToken, nft) = finished_epoch[0]
     (vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
@@ -40,34 +41,40 @@ def test_one_collection_incentive_reward_of_voter(accounts, finished_epoch: tupl
     (zooToken, daiToken, linkToken, nft) = finished_epoch[0]
     (vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
 
-    arena.updateInfoAboutVotedtest_incentive_rewards.pyNumber(nft)
-    assert arena.numberOfVotedNftsInCollection(1, nft) > 0
-    assert arena.numberOfVotedNftsInCollection(2, nft) > 0
+    arena.updateInfoAboutStakedNumber(nft)
+    assert arena.numberOfStakedNftsInCollection(1, nft) > 0
+    assert arena.numberOfStakedNftsInCollection(2, nft) > 0
 
     tx = voting.claimIncentiveVoterReward(1, accounts[-1], {"from": accounts[0]})
 
     assert tx.return_value > 0
-    assert zooToken.balanceOf(accounts[-1]) > 0
 
 def test_incentive_reward_in_new_epoch(accounts, finished_epoch: tuple[tuple, tuple]):
     (zooToken, daiToken, linkToken, nft) = finished_epoch[0]
     (vault, functions, governance, staking, voting, arena, listing) = finished_epoch[1]
     
-    tx = voting.claimIncentiveVoterReward(1, accounts[-1], {"from": accounts[0]})
+    tx1 = voting.claimIncentiveVoterReward(1, accounts[-1], {"from": accounts[0]})
 
-    assert tx.return_value > 0
-    assert zooToken.balanceOf(accounts[-1]) > 0
+    assert tx1.return_value > 0
     
     while arena.currentEpoch() < arena.endEpochOfIncentiveRewards():
-        chain.sleep(arena.epochDuration() + 1)
+        chain.sleep(arena.firstStageDuration() + arena.secondStageDuration() + 1)
         chain.mine(1)
-        arena.updateEpoch({"from": accounts[0]})
-        tx = voting.claimIncentiveVoterReward(1, accounts[-1], {"from": accounts[0]})
-        assert tx.return_value > 0
+        arena.pairNft(1)
+        chain.sleep(arena.thirdStageDuration() + 1)
+        chain.mine(1)
+        chain.sleep(arena.fourthStageDuration() + arena.fifthStageDuration() + 1)
+        arena.requestRandom()
+        vault.increaseMockBalance() # Generate yield.
+        arena.chooseWinnerInPair(0)
+        lastEpochOfIncentiveReward = arena.votingPositionsValues(1)["lastEpochOfIncentiveReward"]
+        tx2 = voting.claimIncentiveVoterReward(1, accounts[-1], {"from": accounts[0]})
+        assert arena.votingPositionsValues(1)["lastEpochOfIncentiveReward"] > lastEpochOfIncentiveReward
+        assert tx2.return_value > 0
         
     chain.sleep(arena.epochDuration() + 1)
     chain.mine(1)
     arena.updateEpoch({"from": accounts[0]})    
 
-    tx = voting.claimIncentiveVoterReward(1, accounts[-1], {"from": accounts[0]})
-    assert tx.return_value == 0
+    tx3 = voting.claimIncentiveVoterReward(1, accounts[-1], {"from": accounts[0]})
+    assert tx3.return_value == 0
--- tests/NftStakingPosition/test_claim_reward_from_staking.py
+++ tests/NftStakingPosition/test_claim_reward_from_staking.py
@@ -32,7 +32,10 @@ def test_batch_claim_reward_from_staking(accounts, tokens, battles):
 	(zooToken, daiToken, linkToken, nft, lpZoo) = tokens
 	(vault, functions, governance, staking, voting, arena, listing) = battles
 
+	tokenId = nft.mintNft({"from": accounts[1]}).return_value
+	nft.approve(staking.address, tokenId, {"from": accounts[1]})
+	
 	staking.stakeNft(nft.address, tokenId, {"from": accounts[1]})
 	stakingPositionId = 1
 
-	staking.batchClaimRewardFromStaking([stakingPositionId], accounts[1], {"from": accounts[1]})
+	staking.batchClaimRewardsFromStaking([stakingPositionId], accounts[1], {"from": accounts[1]})
